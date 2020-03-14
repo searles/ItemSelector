eff5e51bedf52b13dea0ccf5c7b17bf7b1a6017c
searles <karl.gmeiner@gmail.com>
2020-03-14 22:37:54 +0100

    Fix height of entries if too long

--- app/src/main/java/at/searles/itemselector/demo/DemoFolder.kt
+++ app/src/main/java/at/searles/itemselector/demo/DemoFolder.kt
@@ -5,8 +5,11 @@ import at.searles.itemselector.model.Folder
 import at.searles.itemselector.model.Item
 
 class DemoFolder(override val key: String, override val children: List<Item>): Folder {
-    override val title: String = "title: $key"
-    override val description: String = "description: $key"
+    override val title: String = "title: $key saf sa fas f as f asdfasdfasdf asdf as dfa sdf asf" +
+            "s af saf asd fas f as f asd fas df as f asfd a g adf sa fsa df asdf as dfas f af"
+    override val description: String = "description: $key very long blablablablabld fgdf" +
+            "adgd  a sadf asd fas fa sfd asdf asd fa sdf as fd asd f sadf sa df asdf as df asd f sadf as" +
+            "sdf saf asf sa fasf"
     override fun setImageInView(imageView: ImageView) {
         // ignore
     }
--- app/src/main/java/at/searles/itemselector/demo/DemoItem.kt
+++ app/src/main/java/at/searles/itemselector/demo/DemoItem.kt
@@ -4,8 +4,10 @@ import android.widget.ImageView
 import at.searles.itemselector.model.Item
 
 class DemoItem(override val key: String): Item {
-    override val title: String = "title: $key"
-    override val description: String = "description: $key"
+    override val title: String = "title: $key sdf sdf sdf sdf sf sf sfd sdf sf sf sdf s df sdf" +
+            " sf sd fs df sf s df sdf sdf s df"
+    override val description: String = "description: $key sdfs dfas f asf saf asfdasdfas fasf as fasf" +
+            " saf saf as f asf as fas dfasfd sadfasf saf"
     override fun setImageInView(imageView: ImageView) {
         // ignore
     }
